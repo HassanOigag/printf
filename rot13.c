e625fc686c53b926b6da5277e559fd5db5c5355f
badreddinesouhar <badrsouhar@gmail.com>
2023-03-28 18:58:29 -0700

    printf

--- rot13.c
+++ rot13.c
@@ -6,26 +6,33 @@
  * @counter: pointer to the lenght of the string
  * Return: length of the printed string
  */
+
 int rot_13(va_list ap, int *counter)
 {
-	int i, counter;
+	int i;
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *s = va_arg(ap, char *);
-
-	for (counter = 0; s[counter]; counter++)
+	
+	for (*counter = 0; s[*counter]; (*counter)++)
 	{
-		if (s[counter] < 'A' || (s[counter] > 'Z' && s[counter] < 'a') || s[counter] > 'z')
-			_putchar(s[counter]);
+		if (s[*counter] < 'A' || (s[*counter] > 'Z' && s[*counter] < 'a') || s[*counter] > 'z')
+		{
+			_putchar(s[*counter]);
+		}
 		else
 		{
-			for (i = 0; i <= 52; i++)
+			char c = s[*counter];
+			if (c >= 'a' && c <= 'z')
 			{
-				if (s[counter] == rot13[i])
-					_putchar(ROT13[i]);
+				c = ROT13[c - 'a' + 13];
 			}
+			else
+			{
+				c = ROT13[c - 'A' + 39];
+			}
+			_putchar(c);
 		}
 	}
-
-	return (counter);
+	return *counter;
 }
