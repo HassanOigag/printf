379a9cdeb4ec08a7b53f456db884b10574913d19
99Hassan20 <hassanoigag0@gmail.com>
2023-03-31 08:54:39 -0700

    fixed betty

--- main.h
+++ main.h
@@ -5,14 +5,14 @@
 #include <stdarg.h>
 #include <limits.h>
 
-int		_putchar(int c);
-int 	putstr(char *s, int all);
-int 	_printf(const char *format, ...);
-void 	select_id(va_list ap, int c, int *counter);
+int	_putchar(int c);
+int	putstr(char *s, int all);
+int	_printf(const char *format, ...);
+void	select_id(va_list ap, int c, int *counter);
 void	putnbr(int n, int *counter);
 void	putnbr_binary(unsigned int n, int *counter);
 void	putnbr_hex(unsigned int n, int isupper, int *counter);
-void 	put_address(unsigned long int n, int *counter);
-void 	putunbr_octal(unsigned int n, unsigned int base, int *counter);
+void	put_address(unsigned long int n, int *counter);
+void	putunbr_octal(unsigned int n, unsigned int base, int *counter);
 
 #endif
